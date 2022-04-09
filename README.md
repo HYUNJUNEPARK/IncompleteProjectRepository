2ecf2b4bb1bc8097e9da9383ed162ebb041eb48e
HYUNJUNEPARK <june2ac4dev@gmail.com>
2022-04-09 19:23:06 +0900

    bottom navigation

---
+++ README.md
@@ -0,0 +1,17 @@
+Firebase Store
+
+Firebase Realtime Database
+
+Firebase Authentication
+
+
+
+///
+UI
+BottomNavigationView
+app:itemRippleColor="@null" : 클릭 시 Ripple 효과를 없앰
+app:itemTextColor="@color/black" : 아이콘 텍스트 컬러 설정
+app:itemIconTint="@drawable/select_menu_color" :
+아이템 선택/해제 시 컬러 설정 가능하며 drawable 에 <selector> 태그 하위 태그로 item 으로 컬러 지정
+`<item android:color="@color/black" android:state_checked="true"/>`
+`<item android:color="@color/gray" android:state_checked="false"/>`
--- app/src/main/java/com/june/daangnmarket/MainActivity.kt
+++ app/src/main/java/com/june/daangnmarket/MainActivity.kt
@@ -2,14 +2,48 @@ package com.june.daangnmarket
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.june.daangnmarket.chatlist.ChatListFragment
 import com.june.daangnmarket.databinding.ActivityMainBinding
+import com.june.daangnmarket.home.HomeFragment
+import com.june.daangnmarket.mypage.MyPageFragment
 
 class MainActivity : AppCompatActivity() {
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
+    companion object {
+        val fragmentList = arrayListOf(
+            HomeFragment(),
+            ChatListFragment(),
+            MyPageFragment()
+        )
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+        replaceFragment(fragmentList[0])
+        initFragment()
+
+    }
+
+    private fun initFragment(){
+        binding.bottomNavigationView.setOnNavigationItemSelectedListener {  menu ->
+            when (menu.itemId) {
+                R.id.home -> replaceFragment(fragmentList[0])
+                R.id.chatList -> replaceFragment(fragmentList[1])
+                R.id.myPage -> replaceFragment(fragmentList[2])
+            }
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .apply {
+                replace(R.id.fragmentContainer, fragment)
+                commit()
+            }
     }
 }
